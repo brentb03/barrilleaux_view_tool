8c8c006026aa4f4960a1d4ce6a6141184602328d
Brent Barrilleaux <brentbarrilleaux@Brents-MacBook-Pro.local>
2018-10-17 21:30:54 -0500

    Dev tool initial commit

---
+++ lib/barrilleaux_view_tool.rb
@@ -0,0 +1,6 @@
+require "barrilleaux_view_tool/version"
+require "barrilleaux_view_tool/renderer"
+
+module BarrilleauxViewTool
+  # Your code goes here...
+end
---
+++ lib/barrilleaux_view_tool/version.rb
@@ -0,0 +1,3 @@
+module BarrilleauxViewTool
+  VERSION = "0.1.0"
+end
